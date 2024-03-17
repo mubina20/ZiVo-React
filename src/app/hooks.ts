b02b8bc9b5a6084f4f610d5dffe07ec0e47d180c
Mubina <mubina3120@gmail.com>
2024-03-17 14:50:43 +0900

    feat: useMediaQuery create

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,14 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
+import { useMediaQuery } from "./useMediaQuery";
+
+export default function useDeviceSize() {
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
+  const isTablet = useMediaQuery("(min-width:768px) and (max-width:1023px)");
+  const isMobile = useMediaQuery("(max-width:767px)");
+
+  return { isDesktop, isTablet, isMobile };
+}
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,16 +1,27 @@
 import { Header } from '../../components/header/header';
 import { LeftSidebar } from '../../components/sidebars/left_sidebar';
 import { RightSidebar } from '../../components/sidebars/right_sidebar';
+import useDeviceSize from '../../hooks';
 import { Home } from './home';
 
 export function HomePage() {
+	const { isDesktop, isMobile, isTablet } = useDeviceSize();
+	if (isDesktop) {
+		return( 
+			<div>
+				<Header />
+				<LeftSidebar />
+				<Home />
+				<RightSidebar />
+			</div>
+		)
+	}
+	if (isTablet) {
+		return <div style={{color: "white", fontSize: "5rem"}}>isTablet</div>;
+	}
+	if (isMobile) {
+		return <div style={{color: "white", fontSize: "5rem"}}>isMobile</div>;
+	}
 	
-	return (
-		<div className="homepage">
-			<Header />
-            <LeftSidebar />
-			<Home />
-            <RightSidebar />
-		</div>
-	);
+	return <div>Other</div>;
 }
